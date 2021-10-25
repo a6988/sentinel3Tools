e02061bcb078aaffc7d51724d45d56aea27e169f
a6988 <onipochiflute1016.mobile@gmail.com>
2021-10-25 16:03:48 +0900

    shpファイルと一致する地点のデータを読み込むようにした
    geopandasやshapelyを使ってshpファイルと重なる地点のデータを
    読み込むようにした。
    TODO
    geopandasのデータ出力方法を工夫して地点を分かりやすくすること

--- visSentinel3.py
+++ visSentinel3.py
@@ -10,76 +10,11 @@ import glob
 from parameter import *
 import subprocess
 import os
+import pandas as pd
+import geopandas as gpd
+from shapely.ops import cascaded_union
 
 
-def getLatLonArray(geoCoordNCFile:str, lats:list,lons:list):
-    '''
-    指定範囲内の配列を得る
-    '''
-
-    # 指定範囲の最小最大からstart, endを算出
-    startLat = min(lats)
-    endLat = max(lats)
-    startLon = min(lons)
-    endLon = max(lons)
-
-    # netCDF4の読み込み
-    with netCDF4.Dataset(geoCoordNCFile,'r') as geoNC:
-
-        # 緯度・経度で範囲にあるかのフラグ配列
-        inLatFlag = ( geoNC['latitude'][:] > startLat ) & \
-            ( geoNC['latitude'][:] < endLat )
-
-        inLonFlag = ( geoNC['longitude'][:] > startLon ) & \
-            ( geoNC['longitude'][:] < endLon )
-
-        # 緯度経度として範囲に入っているかどうかの判定
-        inAreaFlag = inLatFlag * inLonFlag
-        thisRes = np.where(inAreaFlag == True)
-        
-        # 範囲内にはいっているかどうか緯度経度の結果を格納
-        latRes = thisRes[0]
-        lonRes = thisRes[1]
-        startLat = latRes.min()
-        endLat = latRes.max()
-        startLon = lonRes.min()
-        endLon = lonRes.max()
-        
-
-        return {'startLatIndex':startLat, 'endLatIndex':endLat,
-                'startLonIndex':startLon, 'endLonIndex':endLon}
-
-def getArrays(geoNC,itemNC,itemLabel,regionIndex):
-    '''
-    緯度経度が格納されたgeoNCと項目が格納されたitemNCから
-    対象になる範囲のindexを取得する
-    '''
-
-    startLat = regionIndex['startLatIndex']
-    endLat = regionIndex['endLatIndex']
-    startLon = regionIndex['startLonIndex']
-    endLon = regionIndex['endLonIndex']
-
-    itemArray = itemNC[itemLabel][startLat:endLat,startLon:endLon]
-    
-    latArray = geoNC['latitude'][startLat:endLat, startLon:endLon]
-    lonArray = geoNC['longitude'][startLat:endLat, startLon:endLon]
-    return itemArray,latArray,lonArray
-
-def pickupNearestData(pickupLon, pickupLat,
-        itemArray,latArray,lonArray):
-    '''
-    任意の座標に最も近いポイントを取得する
-    distArray[i,j]に各地点からの距離を算出する
-    '''
-    
-    distArray = np.zeros([itemArray.shape[0],itemArray.shape[1]])
-
-    for i in range(0,itemArray.shape[0]):
-        for j in range(0,itemArray.shape[1]):
-            distArray[i,j] = ( latArray[i,j]-pickupLat)**2 + (lonArray[i,j]-pickupLon)**2
-
-    return distArray
 
 
 class visSentinel():
@@ -92,6 +27,7 @@ class visSentinel():
         self.lons = lons
         self.lats = lats
         self.targetItems = targetItems
+        self.coordinate = coordinate 
         # 画像出力を行う場合
         if self.pngExec == True:
             self.visSHPs = visSHPs
@@ -107,6 +43,7 @@ class visSentinel():
             self.pickupDir = pickupDir
             self.pickupLon = pickupLon
             self.pickupLat = pickupLat
+            self.pickupShps = pickupShps
             # フォルダ作成
             if not os.path.isdir(pickupDir):
                 subprocess.run(['mkdir','-p',pickupDir])
@@ -128,7 +65,8 @@ class visSentinel():
                 if self.pngExec == True:
                     self.makeFigure()
                 if self.pickupExec == True:
-                    self.makeTimeseries()
+                    self.pickupFromShp()
+                    #self.makeTimeseries()
             #except Exception as e:
             #    print(e)
         
@@ -151,11 +89,11 @@ class visSentinel():
 
         itemNC = netCDF4.Dataset(itemNCFile,'r')
 
-        regionIndex = getLatLonArray(geoCoordNCFile,lats,lons)
+        regionIndex = self.getLatLonArray(geoCoordNCFile,lats,lons)
 
         # 緯度・経度・項目の配列を取得
         startLatIndex = regionIndex
-        self.itemArray, self.latArray, self.lonArray = getArrays(
+        self.itemArray, self.latArray, self.lonArray = self.getArrays(
                 geoNC,itemNC,itemLabel,regionIndex)
 
         return
@@ -166,14 +104,11 @@ class visSentinel():
         '''
         ### COLORBAR TICKS
         self.CbarTicks = np.linspace(self.vrange[0], self.vrange[1], 5)
-
-
-
-
         fig = plt.figure()
 
         ax = fig.add_subplot(111,projection=ccrs.PlateCarree())
 
+        # log10の値が入っているのであれば、10**self.itemArrayとすべき
         contour = ax.pcolormesh(
                 self.lonArray, self.latArray, self.itemArray*10, 
                 edgecolor="none", cmap="rainbow", 
@@ -215,7 +150,7 @@ class visSentinel():
         valueはlog10の値らしいので取り敢えず変換する
         '''
         
-        distArray = pickupNearestData(self.pickupLon,self.pickupLat,
+        distArray = self.pickupNearestData(self.pickupLon,self.pickupLat,
                 self.itemArray,self.latArray,self.lonArray)
         nearestArrayIndexes = np.where(distArray==distArray.min())
 
@@ -234,6 +169,110 @@ class visSentinel():
             f.write(f"{thisDate},{thisValue}\n")
 
         return
+
+    def pickupFromShp(self):
+        '''
+        shapeファイルと一致する地点からデータを出力する
+        '''
+        ### PREPARE DATA
+        # 縦ベクトル化
+        LAT   = np.reshape(self.latArray,  
+                ((self.latArray.shape[0]  *  self.latArray.shape[1], 1)))
+        LON   = np.reshape(self.lonArray,  
+                ((self.lonArray.shape[0]  *  self.lonArray.shape[1], 1)))
+        DATA  = np.reshape(self.itemArray, 
+                ((self.itemArray.shape[0] *  self.itemArray.shape[1], 1)))
+        #
+        if LAT.shape[0] != LON.shape[0] or LAT.shape[0] != DATA.shape[0]:
+            raise valueError('緯度・経度・データの配列の大きさが異なります')
+        PDATA = pd.DataFrame(np.zeros((LAT.shape[0], 3)))
+        PDATA.columns = [self.thisTargetItem, "LAT", "LON"]
+        PDATA.loc[:, self.thisTargetItem] = DATA[:,0]
+        PDATA.loc[:, "LAT"] = LAT[:,0]
+        PDATA.loc[:, "LON"] = LON[:,0]
+        PDATA = PDATA.dropna(how="any", axis=0)
+        PDATA = gpd.GeoDataFrame(PDATA, geometry=gpd.points_from_xy(PDATA.LON, PDATA.LAT)).set_crs(self.coordinate)
+        ### PICK DATA
+        for thisPickupShp in self.pickupShps:
+            shp = gpd.read_file(thisPickupShp)
+            shp = cascaded_union(shp.geometry)
+            res = PDATA[PDATA.geometry.within(shp)]
+            # ここにファイルにアペンドしていくようにする必要あり
+            res.to_csv(f"{self.pickupDir}/{pickupResFilename}", 
+                    index=None)
+
+
+    def getLatLonArray(self,geoCoordNCFile:str, lats:list,lons:list):
+        '''
+        指定範囲内の配列を得る
+        '''
+
+        # 指定範囲の最小最大からstart, endを算出
+        startLat = min(lats)
+        endLat = max(lats)
+        startLon = min(lons)
+        endLon = max(lons)
+
+        # netCDF4の読み込み
+        with netCDF4.Dataset(geoCoordNCFile,'r') as geoNC:
+
+            # 緯度・経度で範囲にあるかのフラグ配列
+            inLatFlag = ( geoNC['latitude'][:] > startLat ) & \
+                ( geoNC['latitude'][:] < endLat )
+
+            inLonFlag = ( geoNC['longitude'][:] > startLon ) & \
+                ( geoNC['longitude'][:] < endLon )
+
+            # 緯度経度として範囲に入っているかどうかの判定
+            inAreaFlag = inLatFlag * inLonFlag
+            thisRes = np.where(inAreaFlag == True)
+            
+            # 範囲内にはいっているかどうか緯度経度の結果を格納
+            latRes = thisRes[0]
+            lonRes = thisRes[1]
+            startLat = latRes.min()
+            endLat = latRes.max()
+            startLon = lonRes.min()
+            endLon = lonRes.max()
+            
+
+            return {'startLatIndex':startLat, 'endLatIndex':endLat,
+                    'startLonIndex':startLon, 'endLonIndex':endLon}
+
+    def getArrays(self,geoNC,itemNC,itemLabel,regionIndex):
+        '''
+        緯度経度が格納されたgeoNCと項目が格納されたitemNCから
+        対象になる範囲のindexを取得する
+        '''
+
+        startLat = regionIndex['startLatIndex']
+        endLat = regionIndex['endLatIndex']
+        startLon = regionIndex['startLonIndex']
+        endLon = regionIndex['endLonIndex']
+
+        # Sentinel3のデータは(少なくともCHLAは)log10の値として入っている
+        if self.thisTargetItem in ['CHLA','TSM']:
+            itemArray = 10 ** (itemNC[itemLabel][startLat:endLat,startLon:endLon])
+        
+        latArray = geoNC['latitude'][startLat:endLat, startLon:endLon]
+        lonArray = geoNC['longitude'][startLat:endLat, startLon:endLon]
+        return itemArray,latArray,lonArray
+
+    def pickupNearestData(self,pickupLon, pickupLat,
+            itemArray,latArray,lonArray):
+        '''
+        任意の座標に最も近いポイントを取得する
+        distArray[i,j]に各地点からの距離を算出する
+        '''
+        
+        distArray = np.zeros([itemArray.shape[0],itemArray.shape[1]])
+
+        for i in range(0,itemArray.shape[0]):
+            for j in range(0,itemArray.shape[1]):
+                distArray[i,j] = ( latArray[i,j]-pickupLat)**2 + (lonArray[i,j]-pickupLon)**2
+
+        return distArray
+            
 
 if __name__ == '__main__':
 
